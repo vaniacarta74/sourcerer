fd5b8064130d24cb2f2b3d6ef999b7f0c9d307a5
adm-gfattori <adm-gfattori@devserverspt.enas.local>
2022-04-07 12:46:56 +0200

    Correzioni al codice per la gestione degli url in index.php

--- src/config/config.php
+++ src/config/config.php
@@ -6,5 +6,5 @@ require_once('php_Sourcerer.inc.php');
 $now = new \DateTime('NOW', new \DateTimeZone('Europe/Rome'));
 define('START', $now->format('Y-m-d H:i:s.u'));
 
-define('DEBUG_LEVEL', 1);
+define('DEBUG_LEVEL', 2);
 define('ERROR_LOG', __DIR__ . '/../../log/error.log');
--- src/index.php
+++ src/index.php
@@ -10,7 +10,7 @@ try {
     $uri = filter_input(INPUT_SERVER, 'REQUEST_URI');
     
     if ($file) {        
-        $taditional = 'src/index.php?file=' . $file;        
+        $taditional = 'index.php?file=' . $file;        
         $restful = 'api/' . $file . '/?';
         $url = str_replace($taditional, $restful, $uri);
     } else {    
@@ -20,11 +20,11 @@ try {
     $path = strtok($url, '?');
     
     $router = new Router($path); 
-    $route = $router->getFile();
+    $route = $router->getFile();  
     
     header('Location: http://' . SITPIT_HOST . $route);
     
 } catch (\Exception $e) {
-    Error::errorHandler($e, DEBUG_LEVEL, 'cli');
+    Error::errorHandler($e, DEBUG_LEVEL, 'html');
     exit();
 }
